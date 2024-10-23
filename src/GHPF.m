69ad505ff9ced2ac545a409b57d1440b688e6c6d
Muhammad Equilibrie Fajria <104043362+MuhLibri@users.noreply.github.com>
2024-10-23 23:16:22 +0700

    feat: add IHPF, GHPF, and BHPF

---
+++ src/GHPF.m
@@ -0,0 +1,41 @@
+function GHPF(image, D0)
+    f = im2double(image);
+    [M,N] = size(image);
+    P = 2*M;
+    Q = 2*N;
+
+    % Fourier transform and padding at the same time    
+    F = fft2(f, P, Q);
+    % Shift the image    
+    F = fftshift(F);
+    S2 = log(1+abs(F));
+    figure, imshow(S2,[]); title('Fourier spectrum');
+
+    % Set up range of variables.
+    u = 0:(P-1);
+    v = 0:(Q-1);
+    % Compute the indices for use in meshgrid
+    idx = find(u > P/2);
+    u(idx) = u(idx) - P;
+    idy = find(v > Q/2);
+    v(idy) = v(idy) - Q;
+
+    % Compute H
+    [V, U] = meshgrid(v, u);
+    D = sqrt(U.^2 + V.^2);
+    H = exp(-(D.^2)./(2*(D0^2)));
+    
+    % 1 - H because it's high pass filter    
+    H = 1 - H;
+    H = fftshift(H); figure;imshow(H);title('LPF Gaussian Mask');
+    figure, mesh(H);
+    
+    G = H.*F;
+    G1 = ifftshift(G);
+    
+    G2 = real(ifft2(G1));
+    figure; imshow(G2);title('output image after inverse 2D DFT');
+    
+    G2 = G2(1:M, 1:N);
+    figure, imshow(G2); title('output image');
+end
