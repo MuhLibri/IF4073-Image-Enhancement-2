da80af14d7fdc0edd29a7adb274fc9b5ae1a4c27
Edia Zaki <13521141@mahasiswa.itb.ac.id>
2024-10-25 23:52:19 +0700

    feat: image smoothing & blurring GUI

--- src/gaussianFilter.m
+++ src/gaussianFilter.m
@@ -1,3 +1,3 @@
 function img = gaussianFilter(image, filter_dim, sigma)
-    img = convolution(image, fspecial("gaussian", [filter_dim, filter_dim], sigma));
+    img = uint8(convolution(image, fspecial("gaussian", [filter_dim, filter_dim], sigma)));
 end
--- src/meanFilter.m
+++ src/meanFilter.m
@@ -1,3 +1,3 @@
 function img = meanFilter(image, filter_dim)
-    img = convolution(image, (ones(filter_dim) / (filter_dim^2)));
+    img = uint8(convolution(image, (ones(filter_dim) / (filter_dim^2))));
 end
