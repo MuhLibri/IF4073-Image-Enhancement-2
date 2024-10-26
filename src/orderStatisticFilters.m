0fd1e5ad8b8f3973b024cd84e98c3974a3b0f490
Edia Zaki <13521141@mahasiswa.itb.ac.id>
2024-10-26 23:16:36 +0700

    chore: cleanup & add image

--- src/orderStatisticFilters.m
+++ src/orderStatisticFilters.m
@@ -1,18 +1,14 @@
 function img = orderStatisticFilters(image, filter_dim, type, d)
-    % Validate the filter type
     if ~(strcmp(type, 'max') || strcmp(type, 'min') || strcmp(type, 'median') || ...
          strcmp(type, 'alpha-trimmed') || strcmp(type, 'midpoint'))
         error('Invalid filter type. Supported types are: max, min, median, alpha-trimmed, midpoint.');
     end
 
-    % Set default value for d in case it's not provided
     if nargin < 4
         d = 0;
     end
 
     [img_rows, img_cols, img_dims] = size(image);
-
-    % Define filter center
     filter_center = floor((filter_dim + 1) / 2);
     
     img = zeros(img_rows, img_cols, img_dims);
@@ -20,12 +16,10 @@ function img = orderStatisticFilters(image, filter_dim, type, d)
     for i = 1:img_rows
         for j = 1:img_cols
             for k = 1:img_dims
-                % If near the boundary, copy the original pixel
                 if (i - (filter_center - 1) < 1 || j - (filter_center - 1) < 1 || ...
                     i + (filter_center - 1) > img_rows || j + (filter_center - 1) > img_cols)
                     img(i, j, k) = image(i,j,k);
                 else
-                    % Extract the window of pixels
                     window = image(i-(filter_center - 1):i+(filter_center - 1), ...
                                    j-(filter_center - 1):j+(filter_center - 1), k);
                     
@@ -39,16 +33,9 @@ function img = orderStatisticFilters(image, filter_dim, type, d)
                         case 'midpoint'
                             img(i, j, k) = (max(double(window), [], "all") + min(double(window), [], "all")) / 2;
                         case 'alpha-trimmed'
-                            % Sort the values in the window
                             sorted_window = sort(double(window(:)));
-            
-                            % Determine the number of elements to trim
                             trim_d = min(d, floor(length(sorted_window) / 2));
-                            
-                            % Apply trimming
                             trimmed_window = sorted_window(1 + trim_d:end - trim_d);
-            
-                            % Calculate the mean of the trimmed window
                             img(i, j, k) = mean(trimmed_window);
                     end
                 end
